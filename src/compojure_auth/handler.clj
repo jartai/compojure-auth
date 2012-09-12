0477f4426b41b1f208ff5952e43907670ba61072
owainlewis <owain@owainlewis.com>
2012-09-12 21:04:40 +0100

    working through sessions

--- src/compojure_auth/handler.clj
+++ src/compojure_auth/handler.clj
@@ -7,19 +7,21 @@
             [compojure.route :as route]))
 
 (defn login-user
-  [params]
+  [params session]
   (let [username (get params "username")
         password (get params "password")]
   (if (user/exists? username password)
-    "Found user"
+    (do
+      (assoc session :user_id 1)
+      (str "Found user" session))
     "No user found")))
 
 (defroutes app-routes
   (GET "/" {session :session} (layout/login session))
-  (POST "/login" {form-params :form-params} (login-user form-params))
+  (POST "/login" {session :session form-params :form-params} (login-user form-params session))
                                                        
   (route/not-found "Not Found"))
 
 (def app
   (-> (handler/site app-routes)
-      session/wrap-session))
+      session/wrap-session app-routes))
