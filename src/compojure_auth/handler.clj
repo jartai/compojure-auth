87d564b444b887530c30ee3753217301108ced38
owainlewis <owain@owainlewis.com>
2012-09-24 13:39:06 +0100

    updated middleware

--- src/compojure_auth/handler.clj
+++ src/compojure_auth/handler.clj
@@ -1,7 +1,8 @@
 (ns compojure-auth.handler
   (:use [compojure.core]
         [ring.middleware.session.memory :only [memory-store]]
-        [compojure-auth.models.user :only (exists?)])
+        [compojure-auth.models.user :only (exists?)]
+        [ring.middleware.params :only [wrap-params]])
   (:require [compojure.route :as route]
             [noir.session :as session]
             [ring.adapter.jetty :as jetty]
@@ -12,25 +13,34 @@
 
 ;; Helper methods
 
-(defn logging-middleware [app]
+(defn logging-middleware
+  "Generic logging middleware"
+  [app]
   (fn [req]
+    (println
+      (str "\nBegin "
+      (get req :request-method)
+      " "
+      (get req :uri) "\n"))
     (println req)
     (app req)))
 
 (defn logged-in? []
-  (if (session/get :user)
+  (if (session/get :user_id)
     true
     false))
 
-(defn login [user password]
-  ;; check if the user exists, if so add user id to session
-  ;; TODO encryption, flash messages
-  (if (exists? "owainlewis" "testing")
-    (do (session/put! :user user)
+(defn login [params]
+  (let [user (get params "user")
+        pass (get params "password")]
+    ;; check if the user exists, if so add user id to session
+    ;; TODO encryption, flash messages
+  (if (exists? user pass)
+    (do (session/put! :user_id 1)
         (response/redirect "/"))
     (do
       (session/flash-put! :errors "Invalid user")
-      (response/redirect "/login"))))
+      (response/redirect "/login")))))
 
 (defn index-page
   []
@@ -39,25 +49,30 @@
     "Please log in <a href='/login'>Login</a>"))
 
 (defn  logout []
-   (do (session/remove! :user)
+   (do (session/remove! :user_id)
        (response/redirect "/")))
 
 (defroutes resource-routes
   (-> (route/resources "/*")))
 
-(defroutes app-routes
+(defroutes auth-routes
+  
   (GET "/" [] (index-page))
+  
   (GET "/login" []
     (layout/login-form))
-  (POST "/login" {{:keys [user password]} :form-params}
-    (login user password))
+  
+  (POST "/login" {params :params}
+    (login params))
+  
   (GET "/logout" []
-       (logout))
+    (logout))
+  
   (route/resources "/*")
   (route/not-found "Not Found"))
 
 (def app
-  (-> app-routes
+  (-> (wrap-params auth-routes)
       logging-middleware
       (session/wrap-noir-session {:store (memory-store)})
       (session/wrap-noir-flash)))
